b8fcee21fedd2c80c06e37b90583edfaec40c639
Biswanath Das <cv.biswanath@gmail.com>
2022-10-09 10:39:19 +0530

    ok

--- endpoints/GetRawConfig.php
+++ endpoints/GetRawConfig.php
@@ -1,19 +1,31 @@
 <?php
 include '../config/query.php';
 
-$datas = [
-    'conditions' => [
-        'status' => 1,
-    ],
-];
-$get_data = select('config', $datas);
+if ($_SERVER["PHP_AUTH_USER"] === "block-admin" && $_SERVER["PHP_AUTH_PW"] === "vTzk1do3Qn!g"){
+    $datas = [
+        'conditions' => [
+            'status' => 1,
+        ],
+    ];
+    $get_data = select('config', $datas);
 
-echo json_encode($get_data[0]);
+    echo json_encode($get_data[0]);
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json; charset=UTF-8');
+    header('Access-Control-Allow-Methods: *');
+    header('Access-Control-Max-Age: 3600');
+    header(
+        'Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+    );
+}
+else{
+    echo "Access denied";
+    http_response_code(401);
+    return;
+}
+   
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Max-Age: 3600');
-header(
-    'Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
-);
+
+
+
